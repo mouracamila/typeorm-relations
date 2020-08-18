56401db846c9f85286d7c429234d0c43082b030d
Camila Moura <camilapaleo@gmail.com>
2020-08-18 13:56:25 +0200

    work in progress entities, table relations

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -9,19 +9,26 @@ import {
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('products')
 class Product {
   id: string;
 
+  @Column()
   name: string;
 
+  @Column({ type: 'numeric', precision: 10, scale: 2 })
   price: number;
 
+  @Column('integer')
   quantity: number;
 
+  @OneToMany(() => OrdersProducts, order => order.product)
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
